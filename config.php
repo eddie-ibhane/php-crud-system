dd1cacd315f72b907e1b416b4b0071ce676b9f21
Eddie Ibhane <enduranceibhanehimin@gmail.com>
2021-03-18 07:54:41 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'eddie');
-define('DB_PASSWORD', 'eliteDB');
-define('DB_NAME', 'employee');
+define('DB_USERNAME', 'db_username');
+define('DB_PASSWORD', 'db_password');
+define('DB_NAME', 'db_name');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
